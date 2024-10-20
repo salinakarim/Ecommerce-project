c313621b6c2cdcccc048b78718777df781212498
unknown <salina6656@gmail.com>
2024-10-20 14:56:02 +0600

    create evommerce website

---
+++ script.js
@@ -0,0 +1,10 @@
+let serchForm =document.querySelector('.search-form');
+document.querySelector('#search-btn').onclick = () =>
+shoppingCart.classList.toggle('active');
+
+
+    
+document.querySelector('#search-btn').onclick = () =>{
+shoppingCart.classList.toggle('active');
+
+}
